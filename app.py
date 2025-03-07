22ce916f7dd46926ceb11d09510821b2447c8dcb
KunwarHemKireet <113333480+KunwarHemKireet@users.noreply.github.com>
2025-03-08 00:02:26 +0530

    2nd time Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,26 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
+import nltk
+import os
+
+# .....................
+# Define local directory to store NLTK data
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_path, exist_ok=True)
+
+# Add this directory to NLTK data path
+nltk.data.path.append(nltk_data_path)
+
+# Ensure 'punkt' is available
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt", download_dir=nltk_data_path)
+# ......................
+
+
+
 nltk.download('punkt') 
 
 from nltk.stem.porter import PorterStemmer
